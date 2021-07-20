b5c88c014275e9f76e12da41d93e36dbc9925fb6
suman <sumandari.ee@gmail.com>
2021-07-20 23:23:42 +0800

    passing data to componen #0

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,17 +6,16 @@ class Counter extends Component {
   };
 
   handleIncrement = (product) => {
-    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 
   render() {
-    const product = { id: 3 };
+    console.log("props", this.props);
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement(product)}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
