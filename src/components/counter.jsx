ddd73841e6d544ac1cd1d731ace6ae090c07a20b
suman <sumandari.ee@gmail.com>
2021-07-20 19:40:19 +0800

    styling dynamic

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,11 +6,11 @@ class Counter extends Component {
   };
 
   render() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return (
       <div>
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={classes}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
