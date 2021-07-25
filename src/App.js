eba0b18fb47691f4c63134807f3a6da5abadf1c0
suman <sumandari.ee@gmail.com>
2021-07-25 15:51:45 +0800

    lifting up componeet

--- src/App.js
+++ src/App.js
@@ -4,15 +4,55 @@ import Counters from './components/counters';
 import './App.css';
 import Counter from './components/counter';
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 1 },
+      { id: 2, value: 2 },
+      { id: 3, value: 2 },
+      { id: 4, value: 4 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    // set state counters dengan value const counters
+    // this.setState({ counters: counters });
+    // juga bisa menggunkan begini
+    this.setState({ counters });
+  };
+
+  render(){
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset} 
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
